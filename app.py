6e65beefae4d3104a2cbc859054303575f4c0583
Oziach <bankheleishan@gmail.com>
2025-03-15 20:13:13 +0530

    Backend functionality done

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from model import load_model, load_data
 from utils import compute_similarity, get_top_matches
 from routes import routes
 
 # Initialize Flask app
 app = Flask(__name__)
+
+
+# Allow same-origin access
+CORS(app, resources={r"/*": {"origins": "*"}})  # "*" allows all origins
 
 #load model
 load_model()

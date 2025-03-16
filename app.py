9330033da2977e36da4e8b4f892e0de5cb43b452
Oziach <bankheleishan@gmail.com>
2025-03-16 19:29:34 +0530

    -

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from model import load_model, load_data
 from utils import compute_similarity, get_top_matches
 from routes import routes
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -18,4 +19,5 @@ load_model()
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
